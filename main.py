23ada54fa5805699cbb54ff239440d57990a827e
Sestus <sestand2008@gmail.com>
2025-09-24 09:55:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+# Funkcija za učitavanje teksta iz datoteke
+def ucitaj_tekst(filepath):
+    try:
+        # ovdje ide logika za citanje datoteke
+        with open (filepath, "r", encoding="utf-8") as file:
+            sadrzaj=file.read()
+        return sadrzaj
+    except FileNotFoundError:
+        print(f"Datoteka '{filepath}' nije pronađena.")
+        return None # Vratiti ćemo "ništa" ako datoteka ne postoji
+
+# funkcija za pročišćavanje tesksta
+def ocisti_tekst(tekst):
+    # Kod za pročišćavanje teksta ide ovdje
+    tekst = tekst.lower()
+    return tekst
+
+
+
+if __name__ == "__main__":
+    filepath = "tekst.txt"
+    print(f"Učitavam tekst iz datoteke: {filepath}")
+    ucitani_tekst = ucitaj_tekst(filepath)
+    if ucitani_tekst:
+        print("Učitani tekst je:")
+        print(ucitani_tekst)
+    else:
+        print("Greška pri učitavanju datoteke.")
+    ucitani_tekst = ocisti_tekst(ucitani_tekst)
+    if ucitani_tekst:
+        print("Očišćeni tekst je:")
+        print(ucitani_tekst)
+    else:
+        print("Greška pri očišćavanju teksta.")
+
+        
