3df8a73b081399b4b0ee1d5f079bbe7820ef68ad
Sestus <sestand2008@gmail.com>
2025-10-01 09:55:07 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+
+kosarica = {}
+
+print(f"prazna kosarica: {kosarica}")
+print(f"tip: {type(kosarica)}")
+
+#dodavanje elemenata u riječnik
+kosarica['jabuka'] = 15
+kosarica['kreke'] = 23
+kosarica['mandarine'] = 3
+ 
+ #ispis rječnika
+ print(f"Napunjena košarica: {kosarica}")
+
+kosarica['jabuka']=kosarica
